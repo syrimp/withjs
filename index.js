ddf774c622db527d2d759219bd7f6148452c6470
syrimp <86773586+syrimp@users.noreply.github.com>
2021-10-31 11:33:23 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const nowDate = document.getElementsByClassName("date");
+const nowTime = document.getElementsByClassName("time");
+
+function getClock(){
+    const date = new Date();
+    const year = String(date.getFullYear());
+    const month = String(date.getMonth()+1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getMonth()).padStart(2, "0");
+
+    nowDate.innerHTML = `${year}년 ${month}월 ${day}일`;
+    nowTime.innerHTML =`${hours}시 ${minutes}분 ${seconds}초`;
+}
+
+getClock();
+setInterval(getClock, 1000);
